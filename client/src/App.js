8ef8031a21ef0ce659270dcf2c65a181d89396f9
davud600 <davudgorqi@gmail.com>
2022-06-12 18:58:03 +0300

    main

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,41 @@
+import data from './data'
+
 export default function App() {
-  return (
-    <div>
-      <header>
-        <a>amazona</a>
-      </header>
-      <main>list products</main>
-    </div>
-  )
+  return (<div>
+    <header>
+      <a href="/">amazona</a>
+    </header>
+    <main>
+      <h1>Featured Products</h1>
+      <div className="products"> {
+        data.products.map(product => (<div className="product"
+          key={
+            product.slug
+        }>
+          <a href={
+            `/product/${
+              product.slug
+            }`
+          }>
+            <img src={
+                product.image
+              }
+              alt={
+                product.name
+              }/></a>
+          <div className="product-info">
+            <p> {
+              product.name
+            }</p>
+            <p>
+              <strong> {
+                product.price
+              }</strong>
+            </p>
+            <button>Add to cart</button>
+          </div>
+        </div>))
+      } </div>
+    </main>
+  </div>)
 }
