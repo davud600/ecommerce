ab538856cd3c2c02bef6d5b15a3b9c06a3f3a60f
davud600 <davudgorqi@gmail.com>
2022-06-13 15:03:32 +0300

    fetching-data

--- client/src/screens/HomeScreen.js
+++ client/src/screens/HomeScreen.js
@@ -1,12 +1,25 @@
+import {useEffect, useState} from 'react'
 import {Link} from 'react-router-dom'
-import data from '../data'
+import axios from 'axios'
 
 export default function HomeScreen() {
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await axios.get('http://localhost:5000/api/products')
+      setProducts(res.data)
+    }
+
+    fetchData()
+  }, [])
+  console.log(products)
+
   return (
     <div>
       <h1>Featured Products</h1>
       <div className="products">
-        {data.products.map((product) => (
+        {products.map((product) => (
           <div className="product" key={product.slug}>
             <Link to={`/product/${product.slug}`}>
               <img src={product.image} alt={product.name} />
