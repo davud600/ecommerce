beeb74afc09f08ee06679a8d3dd3de372fcc58bd
davud600 <davudgorqi@gmail.com>
2022-06-16 00:12:09 +0300

    db-setup

--- server/server.js
+++ server/server.js
@@ -1,31 +1,29 @@
 import express from 'express'
 import cors from 'cors'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
 import data from './data.js'
+import seedRouter from './routes/seedRoutes.js'
+import productRouter from './routes/productRoutes.js'
+
+dotenv.config()
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log('connected to db')
+  })
+  .catch((err) => {
+    console.log(err.message)
+  })
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/products', (req, res) => {
-  res.send(data.products)
-})
-
-app.get('/api/products/slug/:slug', (req, res) => {
-  const product = data.products.find((x) => x.slug === req.params.slug)
-
-  if (!product) return res.status(404).send({message: 'Product Not Found'})
-
-  res.send(product)
-})
-
-app.get('/api/products/:id', (req, res) => {
-  const product = data.products.find((x) => x._id === req.params.id)
-
-  if (!product) return res.status(404).send({message: 'Product Not Found'})
-
-  res.send(product)
-})
+app.use('/api/seed', seedRouter)
+app.use('/api/products', productRouter)
 
 const port = process.env.PORT || 5000
 
