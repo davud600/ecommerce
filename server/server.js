523a39ec7a85bb52cf8fda4a2b21eecb11383a1b
davud600 <davudgorqi@gmail.com>
2022-06-17 14:49:37 +0300

    build

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import path from 'path'
 import cors from 'cors'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
@@ -32,6 +33,12 @@ app.use('/api/seed', seedRouter)
 app.use('/api/products', productRouter)
 app.use('/api/users', userRouter)
 app.use('/api/orders', orderRouter)
+
+const __dirname = path.resolve()
+app.use(express.static(path.join(__dirname, '/client/build')))
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'))
+})
 
 app.use((err, req, res, next) => {
   res.status(500).send({message: err.message})
